86407e7282e6d23fcf9768a7408d487bcb2fb690
aditya6521 <74780249+aditya6521@users.noreply.github.com>
2020-12-13 22:55:29 +0530

    refractor update

--- index.js
+++ index.js
@@ -1,35 +1,37 @@
+console.log("------- Marvel Multi-verse Quiz---------\n")
 var readlineSync=require('readline-sync');
-const chalk=require('chalk');
-
-console.log("                 "+chalk.bold.green.underline("Welcome to the quiz\n")+chalk.blue("\nthis quiz will test your knowledge about India's First's.\nQuiz consits of 5 questions.\nEach right answer rewards you 100 points.\n"));
-console.log(chalk.green("           ---------------------------------\n"));
-var userName=readlineSync.question(chalk.red("Enter your Name to begin? - "));
-console.log(chalk.red("Welcome ")+chalk.yellow.bold(userName)+chalk.red(",let's begin >>>>> \n"));
-console.log(chalk.green("           ---------------------------------\n"));
+var userName=readlineSync.question("Enter Your Name ");
+console.log("Hello "+userName+", Welcome to the Quiz \n")
+console.log("----------***************-----------");
+console.log("# This Quiz Consists of 8 questions.\n# Every Right answer awards you 100 pionts. \n# No negative points for wrong answers. \n# ENJOY \n ");
+console.log("--------****************-------------\n");
 var score=0;
-function playgame(question,answer){
-  var userAnswer=readlineSync.question(chalk.cyan(question));
+var userConsent=readlineSync.question("Are you ready to play? ");
+if(userConsent==="yes"){
+  console.log("\n\n<<<<<<--------Play--------->>>>>>>\n");
+function playquiz(question,answer){
+  var userAnswer=readlineSync.question(question);
   if(userAnswer===answer){
-    score +=100;
-    console.log(chalk.yellow(questionsNow.ans)+" is the right answer. ");
-    console.log(chalk.greenBright(" Score -> "+score));
+  score=score+100;
+  console.log("Congratulations '"+userAnswer+"' is the right answer");
+  console.log("Your Score is "+score);
+  console.log("-----");}
+  else{
+  console.log("oops...Wrong answer try the next question!!! ")
+  console.log("Your Score is still "+score);
+  console.log("-----");}
   }
-  else {
-    console.log(" oops!! WRONG ANSWER, Please try again");
-    console.log(chalk.red(" Score -> "+score));
-  }}
-  var questions=[
-    {question:"Q.1 Who was the First Indian to go into Space? \n a. Kalpana Chawla\n b. Rakesh Sharma\n c. tanay Pratap\n\n Enter Answer(a/b/c) - ",answer:"b",ans:"Rakesh Sharma"},
-     {question:"\n\nQ2. Who is the 1st female IPS officer in India?\n a. Kiran Bedi\n b. Sonia narang\n c. Aparajita Rai\n\n Enter Answer(a/b/c) - ",answer:"a",ans:"Kiran Bedi"},
-     {question:"\n\nQ3. Who was the first Indian to win an Academy(Oscar) Award?\n a. A.R. Rahman\n b. Bhanu Athiya\n c. Gulzar\n\n Enter Answer(a/b/c) - ",answer:"b",ans:"Bhanu Athiya"},
-     { question:"\n\nQ4. Who was the First Indian to win a Nobel prize?\n a. Shubash chandra Bose\n b. J.R.D Tata\n c. Rabindranath Tagore\n\n Enter Answer(a/b/c) - ",answer:"c",ans:"Rabindranath Tagore"},
-     {question:"\n\nQ5. Who was the first woman to receive the Bharat Ratna?\n\n a. Indira Gandhi\n b. Lata Mangeshkar\n c. Mother Teresa\n\n Enter Answer(a/b/c) - ",answer:"a",ans:"Indira Gandhi"}
-  ]
-  for(var i=0;i<questions.length;i++){
-    var questionsNow=questions[i];
-    playgame(questionsNow.question,questionsNow.answer);
-  }
-  console.log(chalk.blue("\n\nYour Total Score = ")+chalk.greenBright(score)+chalk.red.bold ("\n Thanks for Playing"));
-    
-    
-    
+  
+  playquiz("Q.1- In which year the movie Iron Man released?\n ","2008");
+  playquiz("Q.2- What is the name of Thor's Hammer? \n","mjolnir");
+  playquiz("Q.3- What is Captain America's shield made of?\n ","vibranium",);
+  playquiz("Q4.- The Flerkens are a race of extremely dangerous aliens that resembles what?\n ","cat");
+  playquiz("Q5.- what was the name of Loki's alien army?\n ","chitauri");
+  playquiz("Q6.- Who does Mad titan sacrifice to acquire the Soul stone?\n ","gamora");
+  playquiz("Q7.- How many Infinity Stones are there?\n","6");
+  playquiz("Q8.- what were Tony's last words?\n","i am ironman");
+  console.log("-------Your Final Score is ["+score+"]-------")
+  console.log("Thanks "+userName+" for playing. \n Hope you had a fun time. ");}
+  else{
+    if(userConsent==="no")
+    console.log("\nSorry to Hear that...Now Buzz off please");}
